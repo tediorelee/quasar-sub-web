b0176e1822c57884d3ec4d452c836877c1134dc9
TEDIORELEE <tediorelee@qq.com>
2021-05-28 23:34:13 +0800

    add vercel

--- quasar.conf.js
+++ quasar.conf.js
@@ -48,7 +48,6 @@ module.exports = function (/* ctx */) {
 		// Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
 		build: {
 			vueRouterMode: 'history', // available values: 'hash', 'history'
-
 			// transpile: false,
 
 			// Add dependencies for transpiling with Babel (Array of string/regex)
